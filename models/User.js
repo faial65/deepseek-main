c681d3dc544aa05c8f62445d164983b32bcf8d9d
Faisal Ahmed <faisal833194@gmail.com>
2025-09-01 22:54:51 +0600

    user.js changed

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
     {
         _id: { type: String, required: true },
         name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
+        email: { type: String, required: true},
         image: { type: String, required: false }
     },
     {timestamps: true}
