665d8aa048f996af62d61bb3ed4430454ea0c2e8
Faisal Ahmed <faisal833194@gmail.com>
2025-09-05 21:32:10 +0600

    modified AppContext.jsx with new contexts

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { AppContextProvider } from "@/context/AppContext";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -21,8 +22,12 @@ export default function RootLayout({ children }) {
       <AppContextProvider>
     <html lang="en">
       <body
-        className={`${inter.variable}  antialiased`}
-      >
+        className={`${inter.variable}  antialiased`}>
+        <Toaster toastOptions={
+          {
+            success:{style:{background:"black",color:"white"}},
+            error:{style:{background:"red",color:"white"}}
+        }}/>
         {children}
       </body>
     </html>
--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,14 +1,75 @@
 "use client";
-import { useUser } from "@clerk/nextjs";
-import { createContext,useContext } from "react";
+import { useAuth, useUser } from "@clerk/nextjs";
+import { headers } from "next/headers";
+import { createContext,use,useContext } from "react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
   const { user } = useUser();
+  const{getToken}=useAuth();
 
+  const [chats,setChats]=useState([]);
+  const [selectedChat,setSelectedChat]=useState(null);
+
+  const createNewChat=async()=>{
+    try{
+      if(!user) return null;
+
+      const token=await getToken();
+
+      await axios.post('/api/chat/create',{},{headers:{
+        Authorization:`Bearer ${token}`
+      }})
+
+      fetchUsersChats();
+    }catch(error){
+      toast.error(error.message);
+    }
+  }
+  const fetchUsersChats=async()=>{
+    try{
+      const token = await getToken();
+      const {data} = await axios.post('/api/chat/get', {headers:{
+        Authorization:`Bearer ${token}`
+      }})
+      if(data.success){
+        console.log(data.data);
+        setChats(data.data);
+
+        //If the user has no chats, create a new chat
+        if(data.data.length===0){
+          createNewChat();
+          return fetchUsersChats();
+        }else{
+          //sort chats by updated date
+          data.data.sort((a,b)=> new Date(b.updatedAt)-new Date(a.updatedAt));
+
+          //set recently updated chat as selected chat
+          setSelectedChat(data.data[0]);
+          console.log(data.data[0]);
+        }
+      }else{
+        toast.error(data.message);
+      }
+    }catch(error){
+      toast.error(error.message);
+    } 
+  }
+
+useEffect(()=>{
+  if(user){
+    fetchUsersChats();
+  }
+},[user]);
   const value = {
-    user
+    user,
+    chats,
+    setChats,
+    selectedChat,
+    setSelectedChat,
+    fetchUsersChats,
+    createNewChat
   }
   return (
     <AppContext.Provider value={value}>
